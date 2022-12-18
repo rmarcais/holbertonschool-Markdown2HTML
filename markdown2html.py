9058642ff956dc452ca31a41fe8f26148485683e
Rémi Marçais <remi.marcais87@gmail.com>
2022-12-19 00:52:00 +0100

    Fixed bugs

--- markdown2html.py
+++ markdown2html.py
@@ -22,11 +22,11 @@ def parse(line, type, isPreviousP):
         line = line.replace(item[1], "<em>" + item[1][2:-2] + "</em>")
     for item in re.findall(r"(^|[\s]|)(__[^\s_][^_]*[^\s_]__)", line):
         line = line.replace(item[1], "<em>" + item[1][2:-2] + "</em>")
-    for item in re.findall(r"\(\([\w\s,\.]+\)\)", line):
+    for item in re.findall(r"\[\[[\S][\w\s,\.]*[\S]\]\]", line):
+        line = line.replace(item, hashlib.md5(item[2:-2].encode()).hexdigest())
+    for item in re.findall(r"\(\([\S][\w\s,\.]+[\S]\)\)", line):
         line = line.replace(item, item[2:-2].translate({ord('c'): None,
                                                         ord('C'): None}))
-    for item in re.findall(r"\[\[[\w\s,\.\(\)]*\]\]", line):
-        line = line.replace(item, hashlib.md5(item[2:-2].encode()).hexdigest())
 
     if type == "li":
         headings = line.split()
